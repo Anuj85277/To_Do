04ff8ce77e79bafc89603b6dc1fff2eefae20d43
Anuj Kumar <anujkumar2383@gmail.com>
2024-09-26 15:18:31 +0530

    to-do code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # To_Do
+You can create a To do task 
+You can update/edit a task.
+You can mark as complete a task.
+You can delete a task.
---
+++ index.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("input-box");
+const listcontrainer = document.getElementById("list-container");
+function AddTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listcontrainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listcontrainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listcontrainer.innerHTML);
+}
+function showTask() {
+  listcontrainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
